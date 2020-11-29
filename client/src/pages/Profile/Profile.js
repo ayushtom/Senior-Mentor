dbb5106edef8a81b92fbdba4ece350713a1a9869
Sudheer Tripathi <tripathisudheer604@gmail.com>
2020-11-29 16:47:03 +0530

    minor change

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,51 +1,44 @@
-import React ,{useContext,useEffect,useState} from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-import './Profile.css'
-import axios from 'axios'
-import { AuthContext } from '../../context/auth'
+import React, { useContext, useEffect, useState } from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
+import "./Profile.css";
+import axios from "axios";
+import { AuthContext } from "../../context/auth";
 
-
-function Profile(){
+function Profile() {
   const { user } = useContext(AuthContext);
 
+  const [response, setResponse] = useState({});
 
-  const [response, setResponse] = useState({})
-  
-  console.log(user)
-  
-    useEffect(() => {
-      axios.get("http://localhost:4000/profile",user.email)
-      .then((res)=>{
-          const response = res; 
-          setResponse(response); 
+  console.log(user);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/profile", {email : user.email})
+      .then((res) => {
+        const response = res;
+        setResponse(response);
       })
-      .catch((err)=>{
-          console.log(err); 
-      });     
-      //getItems().then(data => setItems(data));
+      .catch((err) => {
+        console.log(err);
+      });
+    //getItems().then(data => setItems(data));
   }, []);
-    
- 
-  return(
-  
-  <Card fluid color="teal">
-      
+
+  return (
+    <Card fluid color="teal">
       <Card.Content>
-      <Image size="mini" src wrapped ui={false} />
-        <Card.Header>{response.first_name} {response.last_name}</Card.Header>
+        <Image size="mini" src wrapped ui={false} />
+        <Card.Header>
+          {response.first_name} {response.last_name}
+        </Card.Header>
         <Card.Meta>
-          <span className='date'>Year of Student: {response.year}</span>
+          <span className="date">Year of Student: {response.year}</span>
         </Card.Meta>
-        <Card.Description>
-          {response.branch}
-        </Card.Description>
+        <Card.Description>{response.branch}</Card.Description>
       </Card.Content>
-      <Card.Content extra>
-       {response.email}
-      </Card.Content>
+      <Card.Content extra>{response.email}</Card.Content>
     </Card>
-  )
-  
+  );
 }
 
-export default Profile
+export default Profile;
