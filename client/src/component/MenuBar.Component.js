bfe296760a565175fed40b55c9f6c8016a597459
Sudheer Tripathi <tripathisudheer604@gmail.com>
2020-11-29 12:21:13 +0530

    view all profiles complete

--- client/src/component/MenuBar.Component.js
+++ client/src/component/MenuBar.Component.js
@@ -1,70 +1,78 @@
-import { set } from 'mongoose'
-import React, { useContext,useState } from 'react'
-import { Menu } from 'semantic-ui-react'
-import {Link} from 'react-router-dom'
-import { AuthContext } from '../context/auth';
-export default function Menubar(){
+import { set } from "mongoose";
+import React, { useContext, useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
+export default function Menubar() {
   const { user, logout } = useContext(AuthContext);
-  const pathname=window.location.pathname;
-  const path= pathname==='/' ? 'home' : pathname.substr(1)
-  const [activeItem,setActiveItem]=useState(path)
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
-  const handleItemClick = (e, { name }) => setActiveItem(name)
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
-
-  
   const menuBar = user ? (
     <Menu pointing secondary size="huge" color="teal">
-    <Menu.Item
+      <Menu.Item
         name="home"
-        active={activeItem === 'home'}
+        active={activeItem === "home"}
         onClick={handleItemClick}
         as={Link}
         to="/"
       />
+      <Menu.Item  
+        name="community"
+        active={activeItem === "community"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/all"
+      />
       <Menu.Menu position="right">
-      <Menu.Item name={user.email} active as={Link} to="/profile" />
-      <Menu.Item
+        <Menu.Item name={user.email} active as={Link} to="/profile" />
+        <Menu.Item
           name="chat"
-          active={activeItem === 'chat'}
+          active={activeItem === "chat"}
           onClick={handleItemClick}
           as={Link}
           to="/live"
         />
         <Menu.Item name="logout" onClick={logout} />
       </Menu.Menu>
-      
     </Menu>
   ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
-        active={activeItem === 'home'}
+        active={activeItem === "home"}
         onClick={handleItemClick}
         as={Link}
         to="/"
       />
-
+      <Menu.Item  
+        name="community"
+        active={activeItem === "community"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/all"
+      />
       <Menu.Menu position="right">
         <Menu.Item
           name="login"
-          active={activeItem === 'login'}
+          active={activeItem === "login"}
           onClick={handleItemClick}
           as={Link}
           to="/login"
         />
         <Menu.Item
           name="register"
-          active={activeItem === 'register'}
+          active={activeItem === "register"}
           onClick={handleItemClick}
           as={Link}
           to="/register"
         />
-
       </Menu.Menu>
     </Menu>
   );
 
   return menuBar;
 }
-
