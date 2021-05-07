8ea16b0d36b6e25fa2797d12a780b8882726c97b
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-05-07 23:39:33 +0530

    scroll bar styled

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,22 @@ const theme = createMuiTheme({
     }
     
   },
-  
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*::-webkit-scrollbar': {
+          width: '0.4em'
+        },
+        '*::-webkit-scrollbar-track': {
+          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          backgroundColor: 'rgba(0,0,0,.1)',
+          outline: '1px solid slategrey'
+        }
+      }
+    }
+  }
 });
 ReactDOM.render(
   <ThemeProvider theme={theme}>
