8fab1dd15f1993d11218886d3b1989eb0ee1583a
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-05-07 11:17:33 +0530

    design of chat left part half complete

--- client/src/components/ChatLeft/ChatLeft.jsx
+++ client/src/components/ChatLeft/ChatLeft.jsx
@@ -1,33 +1,89 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import PersonBox from './PersonBox/PersonBox'; 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Box from '@material-ui/core/Box';
+
 
 const useStyles = makeStyles((theme) => ({
     container: {
         margin : theme.spacing(1), 
-        padding: theme.spacing(2),
+        padding: theme.spacing(0),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        overflow : 'auto'
     },
+    list : {
+      width: '100%',
+      backgroundColor: theme.palette.background.paper   
+    }, 
     infobar : {
+      position : "sticky", 
       backgroundColor : "#424242",
       padding : theme.spacing(2),
+      top : theme.spacing(0),
       textAlign : 'center',
-      color : "white"
+      color : "white",
+      marginBottom : theme.spacing(1),
+      zIndex : "1" 
+    },
+    infobarIn : {
+      display : "inline-block"
     }
 }));
+
+const arr = [
+  {
+    chatName : "Thanmay Bhat",
+    lastMessage : "I thought you're gonna call me"
+  },{
+    chatName : "Jeff Bezos",
+    lastMessage : "Teri Alexa kaam nahi kar rahi"
+  },{
+    chatName : "Elon Musk",
+    lastMessage : "SN16 ka improved design teri assistant ko bhej diya hai"
+  },{
+    chatName : "Mukesh Ambani",
+    lastMessage : "Paise kam pad rahe hain do khoha aur bhej"
+  }
+]
 
 const ChatLeft = () => {
     const classes = useStyles();
     return (
         <Container maxWidth="lg" className={classes.container} style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-            <Typography variant="h5" className={classes.infobar}>
-              Chat List
-            </Typography>
+            {/*<Container style={{ position:"fixed" }}> */}
+              <Container className={classes.infobar}>
+                <Box display="flex" justifyContent="space-between"> 
+                  <Box><Link to="/"><ChevronLeftIcon style={{color:"white"}}/></Link></Box>
+                  <Box><Typography variant="h5">Chat List</Typography></Box>
+                </Box>
+              </Container>
+            {/*</Container> */}
             <Typography component="div">
-              Container 1 
+              <List className={classes.list}>
+                {arr.map((x)=>{
+                  return(<>
+                    <PersonBox chatHead={x.chatName} lastMessage={x.lastMessage}/>   
+                    <Divider variant="inset" component="li" />
+                  </>)
+                })}
+                {arr.map((x)=>{
+                  return(<>
+                    <PersonBox chatHead={x.chatName} lastMessage={x.lastMessage}/>   
+                    <Divider variant="inset" component="li" />
+                  </>)
+                })}
+              </List>
             </Typography>
+
+            
         </Container> 
     )
 }
---
+++ client/src/components/ChatLeft/PersonBox/PersonBox.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: '36ch',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+}));
+
+  
+const PersonBox = ({chatHead, lastMessage})=>{
+    const classes = useStyles(); 
+    return (
+        <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary={chatHead}
+          secondary={
+            <React.Fragment>
+              {/* <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                Ali Connors
+              </Typography> */}
+              {lastMessage}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      
+    )
+}
+
+export default PersonBox; 
