f0112922d35b788eb84a7b47c82863918a3e0ec4
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-06-13 23:19:09 +0530

    clean code

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -5,25 +5,13 @@ const { getSocket, setSocket } = require("../controllers/sockets");
 const helpers = require("../helpers/helpers"); 
 
 module.exports = (io) => { 
-    //let x = groupControllers.findOrCreatePCGroup("6092e458237b78237c09d6a3-609a3081f0759a1df8ab317b");
-    // let x =  groupControllers.addPCMessage({
-    //     groupName : "6092e458237b78237c09d6a3-609a3081f0759a1df8ab317b",
-    //     userId : "6092e458237b78237c09d6a3",
-    //     body : "Shakalalaka boom boom"
-    // })
-    //groupControllers.getUserFriendInPC("6092e458237b78237c09d6a3-609a3081f0759a1df8ab317b","6092e458237b78237c09d6a3");
-
     io.use((socket, next) => {
         const token = socket.handshake.auth.token;
-        //onsole.log("Here", token);
         if(token){
             decodeToken(token,(data)=>{
                 if(!data){
                     console.log("Unauthorized socket connection");
                     next(); 
-                    // const err = new Error("not authorized");
-                    // err.data = { content: "Please retry later" }; // additional details
-                    // next(err);
                     
                 } else {
                     socket.userId = data.userId
@@ -32,10 +20,6 @@ module.exports = (io) => {
             });
         } else {
             next();
-            // console.log("No token passed");
-            // const err = new Error("No token passed");
-            // err.data = { content: "Please retry later" }; // additional details
-            // next(err);
         }
         
     });
